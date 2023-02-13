27073aae4e2d59f89f4f0e21282828808efdd1fb
itskoshkin <44982999+itskoshkin@users.noreply.github.com>
2023-02-14 00:47:50 +0300

    Downgrade Android Gradle Plugin to 7.3.0 and fix .gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    id("com.android.application").version("7.4.1").apply(false)
-    id("com.android.library").version("7.4.1").apply(false)
+    id("com.android.application").version("7.3.0").apply(false) //Changed from "7.4.1" for compatibility with Windows-version of Android Studio as of 14th Feb 2023
+    id("com.android.library").version("7.3.0").apply(false) //Changed from "7.4.1" for compatibility with Windows-version of Android Studio as of 14th Feb 2023
     kotlin("android").version("1.8.0").apply(false)
     kotlin("multiplatform").version("1.8.0").apply(false)
 }
